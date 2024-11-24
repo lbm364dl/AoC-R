6c9eaf1b0d941860c5f696f08a2fe84d8f7bd9a1
lbm364dl <geometrydashm364@gmail.com>
2024-11-24 14:47:26 +0100

    Simplify and add comments

--- 2023/d01/d01.r
+++ 2023/d01/d01.r
@@ -3,12 +3,31 @@ library("stringi")
 library("purrr")
 
 find_first <- function(inp, nums) {
+  # create regex with all possible matches ("match1|match2|...")
+  # "|" means "OR" in regex
   pattern <- paste(nums, collapse = "|")
+
   inp |>
-    str_extract_all(pattern) |>
-    map_int(~ match(.x, nums)[1] %% 10)
+    # find first match for each string
+    str_extract(pattern) |>
+    # find its position in nums vector for each found result
+    match(nums) |>
+    # find the number it represents by obtaining modulo 10
+    # nums is especially crafted for this to work, e.g.
+    # "one" is at position 1, "nine" is at position 9,
+    # "1" is at position 11 (which is 1 modulo 10),
+    # "9" is at position 19 (which is 9 modulo 10)...
+    (`%%`)(10)
 }
 
+# regex match with str_extract_all might not work as you think
+# e.g. in "oneight" it would only return "one" because it looks for
+# other matches after the end of current match, i.e.,
+# it finds "one" and then checks the rest of the string "ight",
+# so it does not find "eight". This is why we use a different strategy:
+#   - Find first match in original string ("one" in "oneight")
+#   - Find first reversed match in reversed string ("thgie" in "thgieno")
+# This way we would correctly obtain both "one" and "eight" from "oneight"
 solve <- function(inp, nums) {
   map2_int(
     find_first(inp, nums),
