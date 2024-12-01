22cba3ad92871c531162eb2f56bb357ca200b5ea
lbm364dl <geometrydashm364@gmail.com>
2024-12-01 10:57:12 +0100

    Add dat 1 solution

---
+++ 2024/d01/d01.r
@@ -0,0 +1,26 @@
+library("tibble")
+library("tidyr")
+library("dplyr")
+
+inp <- readLines("2024/d01/input.txt")
+
+df <- tibble(line = inp) |>
+  separate_wider_delim(line, "   ", names = c("n1", "n2")) |>
+  mutate_all(as.integer)
+
+star1 <- tibble(l1 = sort(pull(df, n1)), l2 = sort(pull(df, n2))) |>
+  mutate(abs(l1 - l2), .keep = "none") |>
+  sum()
+
+cnts <- df |>
+  select(n1 = n2) |>
+  group_by(n1) |>
+  summarise(cnt = n())
+
+star2 <- df |>
+  inner_join(cnts, by = "n1") |>
+  mutate(ans = n1 * cnt, .keep = "none") |>
+  sum()
+
+print(str_glue("Star 1: {star1}"))
+print(str_glue("Star 2: {star2}"))
